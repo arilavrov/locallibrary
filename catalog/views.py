bd8786f568519ef75cda38f6b7fc33db244453b7
Artyom <apppptem@gmail.com>
2022-11-20 17:59:44 +0300

    all books and authors pages

--- catalog/views.py
+++ catalog/views.py
@@ -22,3 +22,19 @@ def index(request):
         'index.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_books_include_book':num_books_include_book},
     )
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
